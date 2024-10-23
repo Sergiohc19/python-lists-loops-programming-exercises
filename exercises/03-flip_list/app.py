205068372bfb7fb83f975153df5c7946dffa16a7
Sergio Hernández <161878354+Sergiohc19@users.noreply.github.com>
2024-10-23 10:35:50 +0000

    03 Flip list

--- exercises/03-flip_list/app.py
+++ exercises/03-flip_list/app.py
@@ -1,3 +1,15 @@
 sample_list = [45, 67, 87, 23, 5, 32, 60]
 
 # Your code below
+new_list = []
+
+# for items in reversed(sample_list):
+#     new_list.append(items)
+
+for items in sample_list[::-1]:
+    new_list.append(items)
+
+   
+
+print(sample_list)
+print(new_list)
